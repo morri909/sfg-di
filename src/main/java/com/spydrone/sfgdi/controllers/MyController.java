9621737456cdc13ce51c1d5124f675c4e17e9a0d
Michael Morrison <morri909@umn.edu>
2021-01-14 18:32:14 -0600

    Primary bean example

--- src/main/java/com/spydrone/sfgdi/controllers/MyController.java
+++ src/main/java/com/spydrone/sfgdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package com.spydrone.sfgdi.controllers;
 
+import com.spydrone.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-	public String sayHello() {
-		System.out.println("Hello World");
+	private final GreetingService greetingService;
 
-		return "Hi folks";
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+
+	public String sayHello() {
+		return greetingService.sayGreeting();
 	}
 }
