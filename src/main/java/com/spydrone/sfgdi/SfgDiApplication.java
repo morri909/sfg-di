353ec78051cb29808f271216bc0bc0bc2dcdb0d4
Michael Morrison <morri909@umn.edu>
2021-01-20 20:06:08 -0600

    Spring property example

--- src/main/java/com/spydrone/sfgdi/SfgDiApplication.java
+++ src/main/java/com/spydrone/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package com.spydrone.sfgdi;
 
 import com.spydrone.sfgdi.controllers.*;
+import com.spydrone.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,25 +14,10 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-		System.out.println("---- i18n");
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-		System.out.println(i18nController.sayHello());
-
-		System.out.println("---- Primary bean");
 		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.println(myController.sayHello());
 
-		System.out.println("---- Property");
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("---- Setting");
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("---- Constructor");
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource);
 	}
 
 }
---
+++ src/main/java/com/spydrone/sfgdi/config/PropertyConfig.java
@@ -0,0 +1,35 @@
+package com.spydrone.sfgdi.config;
+
+import com.spydrone.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+	@Value("${spydrone.username}")
+	String user;
+	@Value("${spydrone.password}")
+	String password;
+	@Value("${spydrone.dburl}")
+	String url;
+
+	@Bean
+	public FakeDataSource fakeDataSource() {
+		FakeDataSource fakeDataSource = new FakeDataSource();
+		fakeDataSource.setUser(user);
+		fakeDataSource.setPassword(password);
+		fakeDataSource.setUrl(url);
+		return fakeDataSource;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		return propertySourcesPlaceholderConfigurer;
+	}
+}
