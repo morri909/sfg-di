3ce8e53da1293ede4027a1f58f966ae8b6a3d7d9
Michael Morrison <morri909@umn.edu>
2021-01-20 20:18:14 -0600

    Environment variable example

--- src/main/java/com/spydrone/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/spydrone/sfgdi/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.spydrone.sfgdi.config;
 
 import com.spydrone.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+	@Autowired
+	Environment env;
 
 	@Value("${spydrone.username}")
 	String user;
@@ -21,7 +26,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		fakeDataSource.setUser(env.getProperty("USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
