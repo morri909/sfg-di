ec3f76d3334ddf62d27835792e4aa939fe499518
Michael Morrison <morri909@umn.edu>
2021-01-21 18:30:16 -0600

    Spring application properties

--- src/main/java/com/spydrone/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/spydrone/sfgdi/config/PropertyConfig.java
@@ -2,25 +2,12 @@ package com.spydrone.sfgdi.config;
 
 import com.spydrone.sfgdi.examplebeans.FakeDataSource;
 import com.spydrone.sfgdi.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-		@PropertySource("classpath:datasource.properties"),
-		@PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-	@Autowired
-	Environment env;
 
 	@Value("${spydrone.username}")
 	String user;
@@ -38,7 +25,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(env.getProperty("USERNAME"));
+		fakeDataSource.setUser(user);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
@@ -53,9 +40,4 @@ public class PropertyConfig {
 		return fakeJmsBroker;
 	}
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
 }
